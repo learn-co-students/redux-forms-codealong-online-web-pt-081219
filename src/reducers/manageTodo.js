7ad2254369af83afa61d713a82e7e165847c5ed0
Dalia <dalia.sawaya@flatironschool.com>
2020-05-31 20:06:51 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,32 +2,10 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
     switch (action.type){
-    case ' ADD_TODO':
-
-      console.log({ todos: state.todos.concat(action.payload.text) })
-
-
+    case 'ADD_TODO':
+      console.log({ todos: state.todos.concat(action.payload.text) });
       return {todos: state.todos.concat(action.payload.text)}
       default:
         return state;
   }
-
-
-
-
 }
-
-// export default function manageTodo(state = {
-//   todos: [],
-// }, action) {
-//   switch (action.type) {
-//     case 'ADD_TODO':
- 
-//       ;
- 
-//       return { todos: state.todos.concat(action.payload.text) };
- 
-//     default:
-//       return state;
-//   }
-// }
